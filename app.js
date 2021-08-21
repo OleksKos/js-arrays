1053919ec8391afb32f805fc6dfaf6eb29cbf34d
gidroadmin <maindmn@gmail.com>
2021-08-21 19:37:04 +0300

    Filter

--- app.js
+++ app.js
@@ -36,8 +36,10 @@ let newUsersInc = usersIncludes.filter(item => {
   return item.name.includes('ov');
 })
 console.log(newUsersInc);
+console.groupEnd();
 
 //---------------MAP---------------
+console.group('Method MAP');
 //Служит для того, чтобы переберать массивы
 const temp = [0, 4, 5, 9, 7, 6, 4, 22, 45, 9, -1, -2, -3];//C F = C * 1.8 + 32
 //метод MAP создает новый массив и применяет к нему какие-то действия
@@ -73,10 +75,11 @@ const newBase = base.map(item => {
 })
 
 console.log(newBase);
-
+console.groupEnd();
 
 //----------------------PUSH---------------------------------------------
 //Добавляет элементы в конец массива
+console.group('Method PUSH');
 let mapA = [99, 88];
 console.log(mapA);
 mapA[mapA.length] = 111;
@@ -94,7 +97,9 @@ function hi(b) {
 }
 console.log(hi(88)); // возвращает длину массива, при этом не добавляя элемент
 hi(88);
+console.groupEnd();
 //--------------------------------POP---------------------
+console.group('Method POP');
 //Метод pop удаляет последний элемент в массиве
 mapA.pop();
 console.log(mapA);
@@ -115,4 +120,31 @@ document.querySelector('.pop').onclick = () => {
   console.log('Товар id' + goods + ' был удален');
   console.log(car);
 }
+console.groupEnd();
+//----------------------------Filter--------------------------------
+console.group('Method FILTER');
+const filterArray = [3, 4, 5, 6, 9, 8, 55, 66];
+//Фильтрация исходного массива. В результате работы мы получаем новый массив, прошедший по определнному условию
+// В переменную item попадает каждый элемент по очереди. Если функция Callback возвращает true, то элемент проходит в новый массив
+let newFilterArray = filterArray.filter((item, index) => {
+  if (item > 8) {
+    return true; //Именно true, а не item; После return JS трактует все true или False
+  }
+})
+console.log(newFilterArray);
+//Второй параметр (не обязательный) это index элемента. Третий параметр - сам исходный массив
 
+const clients = [
+  { "lin": "fgfg0rths0d55", "user": "Ivanov", "debt": 4 },
+  { "lin": "fgfg0rths0d158", "user": "Vanov", "debt": 80 },
+  { "lin": "fgfg0rths0d", "user": "Sivanov", "debt": 40 },
+  { "lin": "fgfg0rt", "user": "Nov", "debt": 145 },
+];
+
+// let debts = clients.filter(item => (item.debt > 10)).sort((a, b) => (b.debt - a.debt));
+// console.log(debts);
+//Тоже самое только в две строки
+let debts = clients
+  .filter(item => (item.debt > 10))
+  .sort((a, b) => (b.debt - a.debt));
+console.log(debts);
